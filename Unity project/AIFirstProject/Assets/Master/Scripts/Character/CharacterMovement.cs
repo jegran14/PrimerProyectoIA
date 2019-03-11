c6760eefa55591899848e0b6ade9cbe7392f56fb
Vicent Santamarta <vicent.santamarta@gmail.com>
2019-03-12 00:32:37 +0100

    Implementado movimiento del jugador
    Falta verificar que no hay nigun problema, por el momento, el movimiento es bastante simple pero funcional

---
+++ Unity project/AIFirstProject/Assets/Master/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour
+{
+    public float movementSpeed = 6f;
+    public float turnSpeed = 20f;
+
+    private Rigidbody rb;
+    private Vector3 moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveInput = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Turn();
+    }
+
+    /// <summary>
+    /// This function will receive the vertival and horizontal inputs necessary to move the character
+    /// </summary>
+    /// <param name="h">Input necessary for the movement for the horizontal axis, or 'X' axis</param>
+    /// <param name="v">Input necessary for the movement for the vertical axis, or 'Z' axis</param>
+    public void SetInput(float _horizontal, float _vertical)
+    {
+        moveInput.Set(_horizontal, 0f, _vertical);
+    }
+
+    private void Move()
+    {
+        Vector3 movement = moveInput.normalized * movementSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+
+    }
+
+    /// <summary>
+    /// The character turns towards the movement direction (Make an override from this class if we want to change that)
+    /// </summary>
+    private void Turn()
+    {
+        if(moveInput != Vector3.zero)
+        {
+            Quaternion turn = Quaternion.LookRotation(moveInput.normalized);
+
+            rb.MoveRotation(Quaternion.Lerp(rb.rotation, turn, Time.deltaTime * turnSpeed));
+        }
+    }
+}
---
+++ Unity project/AIFirstProject/Assets/Master/Scripts/Character/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMovement))]
+public class PlayerInput : MonoBehaviour
+{
+    private CharacterMovement movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<CharacterMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.SetInput(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+}
