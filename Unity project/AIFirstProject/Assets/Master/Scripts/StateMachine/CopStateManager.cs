4d7445aad5cf27de273d1165967439a38dc78669
sehiren <33637096+sehiren@users.noreply.github.com>
2019-03-18 20:30:26 +0100

    Prototipo_StateMachine

---
+++ Unity project/AIFirstProject/Assets/Master/Scripts/StateMachine/CopStateManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopStateMachine : MonoBehaviour
+{
+    private StateMachine stateMachine = new StateMachine();
+
+    private void Start()
+    {
+        this.stateMachine.ChangeState(new State_Example());
+        //aqui inicializariamos con el state Patrulla
+    }
+
+    private void Update()
+    {
+        this.stateMachine.ExecuteStateUpdate();
+    }
+
+}
---
+++ Unity project/AIFirstProject/Assets/Master/Scripts/StateMachine/State_Example.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State_Example : IState
+{
+    //aqui podemos segui poninedo avriables privadas
+    //e inicializarlas en el constructor de abajo
+
+    public State_Example(/*GameObject ownerGameObject, tagToLookFor*/)
+    {
+        //constructor
+        //poner aqui cosas como buscar una layer
+        //this.ownerGameObject = ownerGameObject
+        //así lo haces reutilizable para todo tipo de objetos
+    }
+    public void Enter()
+    {
+        
+    }
+
+    public void Execute()
+    {
+      //lo que el estado hace cuando lo ejecutas
+      //en el caso de patrulla, por ejemplo, aquí estaría 
+      //todo el calculo de ruta y mandarlo hacia allí 
+    }
+
+    public void Exit()
+    {
+       
+    }
+}
