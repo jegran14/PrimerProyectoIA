0ef9fdc9ff2b2718e43df651c5d4c62e7a0cb9f5
Vicent Santamarta <vicent.santamarta@gmail.com>
2019-03-18 18:39:06 +0100

    implemented grid and nodes representation

--- Unity project/AIFirstProject/Assets/Master/Scripts/Grid/Grid.cs
+++ Unity project/AIFirstProject/Assets/Master/Scripts/Grid/Grid.cs
@@ -4,20 +4,66 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    public Transform player;
+
     public LayerMask unwalkableMask;
     public Vector2 gridWorldSize;
-    public float nodeRadious;
+    public float nodeRadius;
 
     private Node[,] grid;
+    private float nodeDiameter;
+    private int gridSizeX, gridSizeY;
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+
+
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+        for(int x = 0; x < gridSizeX; x++)
+        {
+            for(int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint);
+            }
+        }
+    }
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
+    {
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
+        return grid[x, y];
+    }
 
     private void OnDrawGizmos()
-    {
-        Vector3 upperLeftCorner = transform.position - new Vector3(16f/2f, 0f, 9f/2f);
-        for(int x = 0; x < 16; x++)
-            for(int y = 0; y < 9; y++)
+    {       
+        if(grid != null)
+        {
+            Node playerNode = NodeFromWorldPoint(player.transform.position);
+            foreach(Node n in grid)
             {
-                Vector3 position = upperLeftCorner + new Vector3(x, transform.position.y, y);
-                Gizmos.DrawCube(position, new Vector3(0.9f, 0.5f, 0.9f));
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if (playerNode == n)
+                    Gizmos.color = Color.blue;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
             }
+        }
     }
 }
