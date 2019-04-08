e43a92ef4bfec4176019a1d285303fb6dcd68cb1
spaceraptor <ggandiamartin@gmail.com>
2019-04-08 19:40:14 +0200

    Diseño y Construcción de Niveles
    Niveles 1, 2 y 3 creados en diferentes escenas dentro de mi carpeta. Falta añadir meta, alarmas y terminar los guardias.
    
    Los guardias no se mueven todos, solo se calcula la ruta de unos pocos!

---
+++ Unity project/AIFirstProject/Assets/Guille/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private GameObject player;
+    private Transform respawnPoint;
+
+    void Start()
+    {
+        respawnPoint = GetComponent<Transform>();
+        player = GameObject.Find("Player");
+        RespawnPlayer();
+    }
+
+    void RespawnPlayer()
+    {
+        Instantiate(player, respawnPoint.position, respawnPoint.rotation);
+    }
+
+}
