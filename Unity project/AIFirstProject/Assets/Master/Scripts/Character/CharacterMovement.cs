13cebad34265657b5ab3e5e1750d4ec881bfcc92
Vicent Santamarta <vicent.santamarta@gmail.com>
2019-04-14 12:34:53 +0200

    simple steering

--- Unity project/AIFirstProject/Assets/Master/Scripts/Character/CharacterMovement.cs
+++ Unity project/AIFirstProject/Assets/Master/Scripts/Character/CharacterMovement.cs
@@ -6,14 +6,11 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     private Rigidbody rb;
-    private LevelChanger sceneScript;
-    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
     }
 
     /// <summary>
